48484603fc8979d4a2c8e7d83ce48a3f9cc0d524
Safarov <elkhan.safarov10@yandex.com>
2020-07-31 22:04:45 +0500

    Создание модульной структуры для хранилища

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import menu from './modules/menu';
+import products from './modules/products';
+import cart from './modules/cart';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-	
+	modules: {
+        menu,
+        products,
+        cart
+    }
 });
